8923dbeaeb69f71173b064ede9f76dc6a425118a
Ben <robinsonbena@gmail.com>
2022-05-30 14:03:46 -0700

    formatted header and alerts on layout.html.erb and added a users controller, users show.html.erb and a route to users#show in routes to create a page for user events.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   resources :events
+  get 'my_events', to: 'users#show'
   root "events#index"
 end
