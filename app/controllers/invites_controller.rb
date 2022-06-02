a567e6eebb0464b5133d93eaf3f3e2ba74b64aa4
Ben <robinsonbena@gmail.com>
2022-06-02 16:50:23 -0700

    created Inivite model and added conditional to views to change event panel colour if event.creator is current user

---
+++ app/controllers/invites_controller.rb
@@ -0,0 +1,18 @@
+class InvitesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+end
